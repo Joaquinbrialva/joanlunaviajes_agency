fa6335bff7ef65870dc225101ec866f6fd8a19ba
joaquinbrialva <joaquinbrialva@gmail.com>
2026-01-03 23:31:43 +0000

    Implementación de Carousel y componetización de CardSection

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,13 +1,25 @@
 import Hero from '@/components/Hero';
-import CardSection from '@/components/CardSection';
 import Footer from '@/components/Footer';
+import CardSection from '@/components/CardSection';
+import Card from '@/components/Card';
+import { generateTrips } from '@/mocks/mockTrips';
+import DestinyCard from '@/components/DestinyCard';
+import { CarouselDemo } from '@/components/Carousel';
 
 export default function Home() {
+	const trips = generateTrips(10)
 	return (
 		<div>
 			<main>
 				<Hero />
-				<CardSection />
+				<CardSection title='Mejores Ofertas'>
+					{trips.map((trip) => (
+						<Card key={trip.id} {...trip} />
+					))}
+				</CardSection>
+				<CardSection title='Próximos Viajes' variant='carousel'>
+					<CarouselDemo />
+				</CardSection>
 				<Footer />
 			</main>
 		</div>
--- frontend/components/CardSection.jsx
+++ frontend/components/CardSection.jsx
@@ -1,51 +1,32 @@
 import '@/styles/CardSection.css';
 import { Button } from './ui/button';
-import Card from './Card';
 import { FaChevronRight } from 'react-icons/fa6';
-import { generateTrips } from '@/mocks/mockTrips';
-import DestinyCard from './DestinyCard';
 
-export default function CardSection() {
-	const trips = generateTrips(6);
-
+export default function CardSection({
+	title,
+	arrow = true,
+	variant = 'grid',
+	children
+}) {
 	return (
 		<div className='section-container'>
 			<div className='section-header'>
 				<div className='section-title'>
-					<h3>Mejores Ofertas Destacadas</h3>
+					<h3>{title}</h3>
 				</div>
-				<div className='section-arrow'>
+				{arrow && (<div className='section-arrow'>
 					<Button>
-						Ver todas las ofertas
+						Ver todas
 						<FaChevronRight />
 					</Button>
-				</div>
+				</div>)
+				}
 			</div>
-			<div className="cards">
-				{trips.map((trip) => (
-					<Card
-						key={trip.id}
-						title={trip.title}
-						location={trip.location}
-						days={trip.days}
-						rating={trip.rating}
-						price={trip.price}
-						image={trip.image}
-						discount={trip.discount}
-						categories={trip.category}
-					/>
-				))}
-			</div>
-			{/* Implementar carousel */}
-			<div className="destiny-section">
-				{trips.map((destiny) => (
-					<DestinyCard
-						key={destiny.id}
-						location={destiny.location}
-						price={destiny.price}
-					/>
-				))}
-			</div>
+			{variant === 'grid' ? (
+				<div className="cards">{children}</div>
+			) : (
+				children
+			)}
 		</div>
 	);
 }
