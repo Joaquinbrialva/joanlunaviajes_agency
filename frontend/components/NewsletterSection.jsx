4cd374e668c4252513ea5ca07bf3f7fdfad2ef90
joaquinbrialva <joaquinbrialva@gmail.com>
2026-01-04 01:17:33 +0000

    Newsletter Section created

---
+++ frontend/components/NewsletterSection.jsx
@@ -0,0 +1,30 @@
+import '@/styles/NewsletterSection.css'
+import { IoMdMail } from "react-icons/io";
+import { InputGroup, TextField, Description } from "@heroui/react";
+import { Button } from './ui/button';
+
+export default function NewsletterSection() {
+    return (
+        <section className="newsletter-section">
+            <div className="newsletter-section_header">
+                <IoMdMail className='newsletter-section_icon' />
+                <h2>No te pierdas ninguna oferta</h2>
+                <p>Suscríbete a nuestro boletín y recibe descuentos exclusivos directamente en tu bandeja de entrada.</p>
+            </div>
+            <div className="email-form">
+                <TextField name="email">
+                    <InputGroup>
+                        <InputGroup.Prefix>
+                            <IoMdMail className='form-icon' />
+                        </InputGroup.Prefix>
+                        <InputGroup.Input className="w-full" placeholder="Tu correo electrónico" />
+                    </InputGroup>
+                </TextField>
+                <Button>Suscribirse</Button>
+            </div>
+            <div>
+                <Description className="email-form_terms" >We'll never share this with anyone else</Description>
+            </div>
+        </section>
+    )
+}
